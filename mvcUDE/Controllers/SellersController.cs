2959ba3fd6705ec634036c2ef9f47fb76d5e0b9d
Tiaguera <tiago_mohaa@hotmail.com>
2020-06-02 18:38:29 -0300

    DateFormating

--- mvcUDE/Controllers/SellersController.cs
+++ mvcUDE/Controllers/SellersController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using mvcUDE.Models;
 using mvcUDE.Models.ViewModels;
 using mvcUDE.Services;
+using mvcUDE.Services.Exceptions;
 
 namespace mvcUDE.Controllers
 {
@@ -43,12 +46,12 @@ namespace mvcUDE.Controllers
         {
             if(id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
         }
@@ -64,14 +67,56 @@ namespace mvcUDE.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                 return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null) 
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(NotFoundException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+            
+        }
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message, RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        };
+            return View(viewModel);
         }
 
     }
--- mvcUDE/Models/ViewModels/ErrorViewModel.cs
+++ mvcUDE/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace mvcUDE.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
