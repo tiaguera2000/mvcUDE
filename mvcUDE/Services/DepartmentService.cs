7aaa4eaed0b0d6d16b89b4f07e5ba9de70da7602
tiago reis <tiago_mohaa@hotmail.com>
2020-06-05 02:34:54 -0300

    async

--- mvcUDE/Services/DepartmentService.cs
+++ mvcUDE/Services/DepartmentService.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace mvcUDE.Services
 {
@@ -14,9 +16,9 @@ namespace mvcUDE.Services
             this.context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return context.Department.OrderBy(x => x.Name).ToList();
+            return await context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
