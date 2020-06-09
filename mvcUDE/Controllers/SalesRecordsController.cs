f1d0070ed15154d5ff2746ca1cf30e97af00f397
Tiaguera <tiago_mohaa@hotmail.com>
2020-06-09 18:41:42 -0300

    SalesRecords

---
+++ mvcUDE/Controllers/SalesRecordsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvcUDE.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSerch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
--- mvcUDE/Controllers/SellersController.cs
+++ mvcUDE/Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 using mvcUDE.Models;
 using mvcUDE.Models.ViewModels;
 using mvcUDE.Services;
@@ -66,8 +67,15 @@ namespace mvcUDE.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(IntegrityException e) 
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
         }
         [HttpGet]
         public async Task<IActionResult> Details(int? id)
