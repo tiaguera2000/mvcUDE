596e963e7ca35f57090dc7aa56eec672f50d5029
Tiaguera <tiago_mohaa@hotmail.com>
2020-06-19 19:05:57 -0300

    ok

--- mvcUDE/Controllers/SalesRecordsController.cs
+++ mvcUDE/Controllers/SalesRecordsController.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using mvcUDE.Services;
 
 namespace mvcUDE.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordService salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            this.salesRecordService = salesRecordService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult SimpleSerch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
+            var result = await salesRecordService.FindByDateAsync(minDate, maxDate);
             return View();
         }
         public IActionResult GroupingSearch()
---
+++ mvcUDE/Services/SalesRecordService.cs
@@ -0,0 +1,32 @@
+﻿using mvcUDE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace mvcUDE.Services
+{
+    public class SalesRecordService 
+    {
+        private readonly mvcUDEContext _context;
+
+        public SalesRecordService(mvcUDEContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<SalesRecords>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+
+            if (minDate.HasValue)result = result.Where(x => x.Date >= minDate.Value);
+            
+            if (maxDate.HasValue) result = result.Where(x => x.Date <= maxDate.Value);
+
+            return await result.Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();//join do sql
+        }
+    }
+}
